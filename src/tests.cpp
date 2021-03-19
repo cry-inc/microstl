eb7f540165e66c71afe0875aa18efe092d3288ff
cry-inc <cry-inc@users.noreply.github.com>
2021-03-19 12:14:16 +0100

    Added test file with a lot of unexpected white space

--- src/tests.cpp
+++ src/tests.cpp
@@ -29,6 +29,29 @@ int main()
 
 	{
 		microstl::MeshParserHandler handler;
+		auto res = microstl::Parser::parseStlFile("../../testdata/crazy_whitespace_ascii.stl", handler);
+		REQUIRE(res == microstl::Parser::Result::Success);
+		REQUIRE(handler.name == "min \t imal");
+		REQUIRE(handler.ascii);
+		REQUIRE(handler.errorLineNumber == 0);
+		REQUIRE(handler.header.empty());
+		REQUIRE(handler.mesh.facets.size() == 1);
+		REQUIRE(handler.mesh.facets[0].n.x == -1);
+		REQUIRE(handler.mesh.facets[0].n.y == 0);
+		REQUIRE(handler.mesh.facets[0].n.z == 0);
+		REQUIRE(handler.mesh.facets[0].v1.x == 0);
+		REQUIRE(handler.mesh.facets[0].v1.y == 0);
+		REQUIRE(handler.mesh.facets[0].v1.z == 0);
+		REQUIRE(handler.mesh.facets[0].v2.x == 0);
+		REQUIRE(handler.mesh.facets[0].v2.y == 0);
+		REQUIRE(handler.mesh.facets[0].v2.z == 1);
+		REQUIRE(handler.mesh.facets[0].v3.x == 0);
+		REQUIRE(handler.mesh.facets[0].v3.y == 1);
+		REQUIRE(handler.mesh.facets[0].v3.z == 1);
+	}
+
+	{
+		microstl::MeshParserHandler handler;
 		auto res = microstl::Parser::parseStlFile("../../testdata/half_donut_ascii.stl", handler);
 		REQUIRE(res == microstl::Parser::Result::Success);
 		REQUIRE(handler.name == "Half Donut");
